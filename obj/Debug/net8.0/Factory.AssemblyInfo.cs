24df604bd8eb75c71524bf34c93dca7392d673b7
Luke J <lukejavan@protonmail.com>
2025-07-08 14:17:34 +0200

    Refactored structure to allow test project and added gitignore.

--- obj/Debug/net8.0/Factory.AssemblyInfo.cs
+++ obj/Debug/net8.0/Factory.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Factory")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b07dbe85dd9a57321459c0ae8784027e3e75c31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Factory")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Factory")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
